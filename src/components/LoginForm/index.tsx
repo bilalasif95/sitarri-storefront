9f3ad331ffcec80eb0317fa3077bbf7938bcfc9f
Bilal Asif <bilal@rnssol.com>
2020-06-15 15:23:41 +0500

    google and facebook login added

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,6 +1,8 @@
 import "./scss/index.scss";
 
 import * as React from "react";
+import FacebookLogin from "react-facebook-login";
+import GoogleLogin from "react-google-login";
 
 import { useSignIn } from "@sdk/react";
 import { maybe } from "@utils/misc";
@@ -22,8 +24,33 @@ const LoginForm: React.FC<ILoginForm> = ({ hide }) => {
     }
   };
 
+  const responseGoogle = response => {
+    console.log(response,'========');
+  };
+
+  const responseFacebook = response => {
+    console.log(response,"====++++++++++");
+  };
+
   return (
     <div className="login-form">
+      <GoogleLogin
+        clientId="501755889014-btls89ktsuijoj5c1lrrjvtr3jmg1fba.apps.googleusercontent.com"
+        buttonText="Continue with google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
+      <FacebookLogin
+        appId="1007814429617488"
+        // autoLoad={true}
+        fields="name,email,picture"
+        callback={responseFacebook}
+        className="facebookLoginButton"
+        textButton="Continue with Facebook"
+        // buttonText="Login"
+        icon="fab fa-facebook-square"
+      />
       <Form
         errors={maybe(() => error.extraInfo.userInputErrors, [])}
         onSubmit={handleOnSubmit}
